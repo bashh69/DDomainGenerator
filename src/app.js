e3994293cf381e14a99a460b02166d4e04c824e7
bashh69 <bashh@hotmail.com>
2022-04-18 22:00:29 +0000

    final commit

--- src/app.js
+++ src/app.js
@@ -1,25 +1,3 @@
-/* eslint-disable */
-// import "bootstrap";
-// import "./style.css";
-
-// import "./assets/img/rigo-baby.jpg";
-// import "./assets/img/4geeks.ico";
-
-// window.onload = () => {
-//   document.querySelector("button").addEventListener("click", () => {
-//     document.querySelector("domain").innerHTML = domainGen();
-//   });
-// };
-// let domainGen = () => {
-//   let pronoun = ["the", "our", "my", "their"];
-//   let adj = ["great", "bif", "best", "worst"];
-//   let noun = ["jogger", "racoon", "friend", "car"];
-//   let end = [".com", ".net", ".org", ".edu"];
-//   console.log(pronoun + adj + noun + end);
-//   return "hello";
-// };
-
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -27,18 +5,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  document.querySelector("#button").addEventListener("click", () => {
-    document.querySelector("#domain").innerHTML = domainGen();
-  });
-};
-let domainGen = () => {
+  let domainGen = [];
   let who = ["Jim", "BillMurray", "Viking", "Monster"];
   let action = ["singing", "attacking", "drinking", "riding"];
   let what = [".com", ".org", ".net", "com.do"];
-
-  let whoIndex = Math.floor(Math.random() * who.length);
-  let actionIndex = Math.floor(Math.random() * action.length);
-  let whatIndex = Math.floor(Math.random() * what.length);
-  return `${who[whoIndex]}${action[actionIndex]}${what[whatIndex]}`;
+  for (let i = 0; i < who.length; i++) {
+    for (let j = 0; j < action.length; j++) {
+      for (let k = 0; k < what.length; k++) {
+        domainGen.push("  " + who[i] + action[j] + what[k]);
+      }
+    }
+  }
+  console.log(domainGen);
+  document.getElementById("domain.results").innerHTML = domainGen;
 };
